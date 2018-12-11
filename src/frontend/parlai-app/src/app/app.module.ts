40903d2df824921433404818a8ff65b220632be7
Dimitris Michailidis <jimichailidis@gmail.com>
2018-12-11 15:45:51 +0100

    Added navigation menu, question compose screen

--- src/frontend/parlai-app/src/app/app.module.ts
+++ src/frontend/parlai-app/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 // Material Animations
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // Material Components
-import { MatButtonModule, MatCardModule, MatInputModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatInputModule, MatMenuModule } from '@angular/material';
 import { UploadComponent } from './upload/upload.component';
 import { QuestionInspectComponent } from './question-inspect/question-inspect.component';
 import { DocumentViewComponent } from './document-view/document-view.component';
@@ -31,7 +31,8 @@ import { StartComponent } from './start/start.component';
     BrowserAnimationsModule,
     MatButtonModule,
     MatCardModule,
-    MatInputModule
+    MatInputModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
