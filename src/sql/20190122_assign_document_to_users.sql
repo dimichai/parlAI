d5934d467bf5b62a0fb32e272868b98dd992a60f
unknown <jeroen@vandenieuwenhof.nl>
2019-01-22 14:27:02 +0100

    Added sql for assigning question docs to users

---
+++ src/sql/20190122_assign_document_to_users.sql
@@ -0,0 +1,15 @@
+INSERT INTO `parlai`.`questionAssignment` (`id`, `docId`, `userId`) VALUES ('2', '9', '9');
+INSERT INTO `parlai`.`questionAssignment` (`id`, `docId`, `userId`) VALUES ('3', '11', '2');
+INSERT INTO `parlai`.`questionAssignment` (`id`, `docId`, `userId`) VALUES ('4', '6', '2');
+INSERT INTO `parlai`.`questionAssignment` (`id`, `docId`, `userId`) VALUES ('5', '49', '8');
+INSERT INTO `parlai`.`questionAssignment` (`id`, `docId`, `userId`) VALUES ('6', '2', '9');
+INSERT INTO `parlai`.`questionAssignment` (`id`, `docId`, `userId`) VALUES ('7', '3', '5');
+INSERT INTO `parlai`.`questionAssignment` (`id`, `docId`, `userId`) VALUES ('8', '4', '10');
+INSERT INTO `parlai`.`questionAssignment` (`id`, `docId`, `userId`) VALUES ('9', '5', '10');
+INSERT INTO `parlai`.`questionAssignment` (`id`, `docId`, `userId`) VALUES ('10', '6', '3');
+INSERT INTO `parlai`.`questionAssignment` (`id`, `docId`, `userId`) VALUES ('11', '22', '4');
+INSERT INTO `parlai`.`questionAssignment` (`id`, `docId`, `userId`) VALUES ('12', '35', '7');
+INSERT INTO `parlai`.`questionAssignment` (`id`, `docId`, `userId`) VALUES ('13', '47', '11');
+INSERT INTO `parlai`.`questionAssignment` (`id`, `docId`, `userId`) VALUES ('14', '41', '13');
+INSERT INTO `parlai`.`questionAssignment` (`id`, `docId`, `userId`) VALUES ('15', '15', '14');
+INSERT INTO `parlai`.`questionAssignment` (`id`, `docId`, `userId`) VALUES ('16', '14', '15');
