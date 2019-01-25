d69be4a753d2dae3c84220e140173acaa6867756
Jeroen van de Nieuwenhof <jeroen@vandenieuwenhof.nl>
2019-01-25 11:36:27 +0100

    Assigned new questions to users

---
+++ src/sql/20190125_assign_new_questions_to_user.sql
@@ -0,0 +1,4 @@
+INSERT INTO `parlai`.`questionAssignment` (`id`, `docId`, `userId`) VALUES ('58', '58', '3');
+INSERT INTO `parlai`.`questionAssignment` (`id`, `docId`, `userId`) VALUES ('59', '59', '3');
+INSERT INTO `parlai`.`questionAssignment` (`id`, `docId`, `userId`) VALUES ('60', '60', '3');
+INSERT INTO `parlai`.`questionAssignment` (`id`, `docId`, `userId`) VALUES ('61', '61', '3');
